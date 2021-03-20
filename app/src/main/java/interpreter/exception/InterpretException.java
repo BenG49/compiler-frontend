ed627d64c41570b1d8306f1c8ae0e9561e138f59
BenG49 <ben@goldfisher.com>
2021-03-20 18:10:16 -0400

    Undo my snap renaming decision

--- app/src/main/java/interpreter/exception/InterpretException.java
+++
@@ -1,8 +0,0 @@
-package interpreter.exception;
-
-public abstract class InterpretException extends Exception {
-    private static final long serialVersionUID = 1L;
-    
-    public abstract String toString();
-
-}
